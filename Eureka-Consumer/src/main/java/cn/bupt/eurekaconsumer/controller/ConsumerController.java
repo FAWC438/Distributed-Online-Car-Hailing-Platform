51a1b4cf9baca1e289ce8d87819cec6d22ac37aa
FAWC-bupt <Kevin_lgh@outlook.com>
2021-06-29 02:04:51 +0800

    完成分布式架构原型

--- Eureka-Consumer/src/main/java/cn/bupt/eurekaconsumer/controller/ConsumerController.java
+++ Eureka-Consumer/src/main/java/cn/bupt/eurekaconsumer/controller/ConsumerController.java
@@ -1,12 +1,17 @@
 package cn.bupt.eurekaconsumer.controller;
 
 import cn.bupt.eurekaconsumer.MyFeignClient;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConsumerController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String uniqueId;
 
     final MyFeignClient myFeignClient;
 
@@ -16,7 +21,14 @@ public class ConsumerController {
     }
 
     @RequestMapping("/hello")
-    public String index() {
+    @SentinelResource
+    public String helloFunction() {
         return myFeignClient.hello();
+    }
+
+    @RequestMapping("/config")
+    @SentinelResource
+    public String configFunction() {
+        return uniqueId;
     }
 }
